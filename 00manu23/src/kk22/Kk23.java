60c6167ca770f8fd79a42bdf2b3b2179c4d613b7
manu <manuel.yanez@connectyourcare.com>
2018-03-05 16:24:34 +0100

    aaad

--- 00manu23/src/kk22/Kk23.java
+++ 00manu23/src/kk22/Kk23.java
@@ -4,7 +4,7 @@ public class Kk23 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print(" \n \n hola mundo \n \n \n");
+		System.out.print(" \n \n hola mundo 2 \n \n \n");
 
 	}
 
